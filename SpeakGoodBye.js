9cd59ee2f2e27aaa66cccd5858861eb2cb2bcc86
Enida Shehu <shehuenida96@gmail.com>
2023-01-04 19:54:49 +0100

    assignment 4

---
+++ SpeakGoodBye.js
@@ -0,0 +1,11 @@
+
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.name = function (name) {
+  console.log(speakWord + " " + name);
+}
+var speakWord = "Good Bye";
+window.byeSpeaker = byeSpeaker;
+
+})(window);
+
